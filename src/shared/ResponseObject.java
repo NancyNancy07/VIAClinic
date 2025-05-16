3716bcb3c04c84938ce7903399be6e1cbc51e304
NancyNancy07 <148770109+NancyNancy07@users.noreply.github.com>
2025-05-16 12:43:34 +0200

    created one method for getAppointment in Response

--- src/shared/ResponseObject.java
+++ src/shared/ResponseObject.java
@@ -7,95 +7,127 @@ import server.model.patientJournal.Diagnosis;
 
 import java.util.List;
 
-public class ResponseObject {
+public class ResponseObject
+{
   private boolean success;
   private String status;
   private String message;
   private int patientId;
+  private Appointment appointment;
   private List<Appointment> appointments;
   private List<Doctor> doctors;
   private List<Patient> patients;
   private Diagnosis diagnosis;
   private List<Diagnosis> diagnoses;
 
-  public ResponseObject() {
+  public ResponseObject()
+  {
   }
 
-  public ResponseObject(boolean success, String message, int patientId) {
+  public ResponseObject(boolean success, String message, int patientId)
+  {
     this.success = success;
     this.message = message;
     this.patientId = patientId;
   }
 
-  public boolean isSuccess() {
+  public boolean isSuccess()
+  {
     return success;
   }
 
-  public void setSuccess(boolean success) {
+  public void setSuccess(boolean success)
+  {
     this.success = success;
   }
 
-  public String getStatus() {
+  public String getStatus()
+  {
     return status;
   }
 
-  public void setStatus(String status) {
+  public void setStatus(String status)
+  {
     this.status = status;
   }
 
-  public String getMessage() {
+  public String getMessage()
+  {
     return message;
   }
 
-  public void setMessage(String message) {
+  public void setMessage(String message)
+  {
     this.message = message;
   }
 
-  public int getPatientId() {
+  public int getPatientId()
+  {
     return patientId;
   }
 
-  public void setPatientId(int patientId) {
+  public void setPatientId(int patientId)
+  {
     this.patientId = patientId;
   }
 
-  public List<Appointment> getAppointments() {
+  public Appointment getAppointment()
+  {
+    return appointment;
+  }
+
+  public void setAppointment(Appointment appointment)
+  {
+    this.appointment = appointment;
+  }
+
+  public List<Appointment> getAppointments()
+  {
     return appointments;
   }
 
-  public void setAppointments(List<Appointment> appointments) {
+  public void setAppointments(List<Appointment> appointments)
+  {
     this.appointments = appointments;
   }
 
-  public List<Doctor> getDoctors() {
+  public List<Doctor> getDoctors()
+  {
     return doctors;
   }
 
-  public void setDoctors(List<Doctor> doctors) {
+  public void setDoctors(List<Doctor> doctors)
+  {
     this.doctors = doctors;
   }
 
-  public List<Patient> getPatients() {
+  public List<Patient> getPatients()
+  {
     return patients;
   }
 
-  public void setPatients(List<Patient> patients) {
+  public void setPatients(List<Patient> patients)
+  {
     this.patients = patients;
   }
 
-  public Diagnosis getDiagnosis() {
+  public Diagnosis getDiagnosis()
+  {
     return diagnosis;
   }
 
-  public void setDiagnosis(Diagnosis diagnosis) {
+  public void setDiagnosis(Diagnosis diagnosis)
+  {
     this.diagnosis = diagnosis;
   }
 
-  public List<Diagnosis> getDiagnoses() {
+  public List<Diagnosis> getDiagnoses()
+  {
     return diagnoses;
   }
 
-  public void setDiagnoses(List<Diagnosis> diagnoses) {
+  public void setDiagnoses(List<Diagnosis> diagnoses)
+  {
     this.diagnoses = diagnoses;
   }
 }
